7ae70779473dd09711b745bb1be57a3f60f7b548
LEE HAK DO <future.fringe@gmail.com>
2013-11-28 10:32:57 +0900

    Signed-off-by: LEE HAK DO <future.fringe@gmail.com>

--- spring-data-jpa/src/com/hedleyproctor/domain/Address.java
+++ spring-data-jpa/src/com/hedleyproctor/domain/Address.java
@@ -6,21 +6,37 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @Entity
-@Data
 public class Address {
+	@Setter
     private Long id;
 
+	@Setter
     private Customer customer;
+	@Setter
+	@Getter
     private String line1;
+	@Setter
+	@Getter
     private String line2;
+	@Setter
+	@Getter
     private String line3;
+	@Setter
+	@Getter
     private String line4;
+	@Setter
+	@Getter
     private String town;
+	@Setter
+	@Getter
     private String county;
+	@Setter
+	@Getter
     private String postcode;
 
     @Id
