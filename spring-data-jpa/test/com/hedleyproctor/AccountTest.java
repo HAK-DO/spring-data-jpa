194a3cd8dda4cf0bb93c488c3ff21bb813855abf
LEE HAK DO <future.fringe@gmail.com>
2013-12-10 10:10:42 +0900

    Signed-off-by: LEE HAK DO <future.fringe@gmail.com>

--- spring-data-jpa/test/com/hedleyproctor/AccountTest.java
+++ spring-data-jpa/test/com/hedleyproctor/AccountTest.java
@@ -3,9 +3,13 @@ package com.hedleyproctor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hedleyproctor.service.AccountService;
 import com.hedleyproctor.service.EntityLoader;
@@ -19,20 +23,26 @@ import config.ApplicationConfig;
 @ContextConfiguration(classes = ApplicationConfig.class)
 public class AccountTest {
 
-	@Autowired
-	EntityLoader entityLoader;
+	@Autowired EntityLoader entityLoader;
 
-	@Autowired
-	AccountService accountService;
+	@Autowired AccountService accountService;
 
 	// @Test
 	public void saveOrUpdateAccount() {
 		entityLoader.load();
 	}
 
-	@Test
+	//	@Test
 	public void findOne() {
 		UserDetails userDetails = accountService.loadUserByUsername("TEST2");
 		System.out.println(userDetails);
+	}
+	
+	@Test
+	@Transactional
+	public void auth(){
+		UserDetails userDetails = accountService.loadUserByUsername("TEST2");
+        Authentication authToken = new UsernamePasswordAuthenticationToken (userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
 	}
 }
